3ac61024cf24fe481b3d6e2bfdc22e11fd67a5ae
Pratik Paliwal <115452006+PratikPaliwal509@users.noreply.github.com>
2026-05-29 12:22:58 +0530

    prisma config

--- prisma.config.js
+++ prisma.config.js
@@ -4,11 +4,8 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
 
   datasource: {
-    // url: process.env.DATABASE_URL,
-    // url: "postgresql://neondb_owner:npg_TGSk4iD7oJze@ep-green-term-adk9flad-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-    url:"postgresql://neondb_owner:npg_hcCatWPdDi57@ep-solitary-grass-ap85h3q2-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-    // url: "postgresql://neondb_owner:npg_TGSk4iD7oJze@ep-green-smoke-adv93fed-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-  },
+    url: process.env.DATABASE_URL,
+    },
 
   migrations: {
     path: "prisma/migrations",
